c09754fcfae525a927072a46f66a52eea52427a4
Kirk <kirkrobertsnyder@gmail.com>
2021-02-15 14:58:35 -0500

    Completed Moods component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -72,7 +72,7 @@ export default function Counter() {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is {(count % 2 == 0) ? 'Even' : 'Odd'}
+        Number {count} is {(count % 2 == 0) ? 'even' : 'odd'}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
